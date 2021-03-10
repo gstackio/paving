15d050e3c07f206853e09829765948cb0b7d8b58
Will Gant <w_gant@protonmail.com>
2021-03-10 15:21:31 +0000

    Use http LB name in tag for healthcheck firewall rule

--- gcp/pas-firewalls.tf
+++ gcp/pas-firewalls.tf
@@ -85,5 +85,5 @@ resource "google_compute_firewall" "http-lb-health-check" {
 
   source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
 
-  target_tags = [google_compute_http_health_check.http-lb.name]
+  target_tags = ["${var.environment_name}-http-lb"]
 }
